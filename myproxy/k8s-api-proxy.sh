675ecf81556697b0b4aa41c1af2d089baf0f1991
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-12-21 09:32:48 -0500

    Create k8s-api-proxy.sh

---
+++ myproxy/k8s-api-proxy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Get the external cluster IP
+EXTERNAL_IP=$(curl -SsL https://kubernetes.default/api |
+ jq -r '.serverAddressByClientCIDRs[0].serverAddress')
+
+# Replace CLUSTER_IP in the rewrite filter and action file
+sed -i "s/CLUSTER_IP/${EXTERNAL_IP}/g"\
+ /etc/privoxy/k8s-rewrite-external.filter
+sed -i "s/CLUSTER_IP/${EXTERNAL_IP}/g"\
+ /etc/privoxy/k8s-only.action
+
+# Start Privoxy un-daemonized
+privoxy --no-daemon /etc/privoxy/config
